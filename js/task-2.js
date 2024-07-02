bfb4361dae589b4f0e59894e49d8763e44fa9985
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-07-02 07:17:26 +0200

    commit task 1,2,5

---
+++ js/task-2.js
@@ -0,0 +1,48 @@
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  }
+];
+
+// 1) Cтворити пустий "ul" список для додавання посилань
+const galleryImgs = document.querySelector("ul.gallery");
+
+// 2) Створення "li" картинок 
+const createGalleryImgs = (images) =>
+  images.map((item) => {
+      const imgEl = document.createElement("img"); // створення картинок
+      imgEl.src = item.url; // наповнення картинки - посилання на картинку
+      imgEl.alt = item.alt; // наповнення картинки - альтернативний текст для картинки
+      imgEl.width = 360; // наповнення картинки - ширина картинки
+      imgEl.height = 300; // наповнення картинки - довжина картинки
+      return imgEl; // вивід в консоль - бо це функція
+    });
+
+
+    const galleryEls = createGalleryImgs(images); // 3) групування картинок 1.li...  2.li...  3.li... в одне ціле
+    console.log(galleryEls); 
+
+    galleryImgs.append(...galleryEls); // 4) Додавання картинок до списку
+
+
---
+++ js/task-5.js
@@ -0,0 +1,23 @@
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)//використовується для генерації випадкового цілого числа в діапазоні від 0 до 16777214 (включно). Число 16777215 (або 2^24 - 1) є найбільшим 24-бітним цілим числом, яке зазвичай використовується для представлення кольорів у форматі RGB.
+    .toString(16) //перетворення числа в шістнадцятковий рядок
+    .padStart(6, 0)}`; //Додає нулі на початку рядка, щоб гарантувати, що результат завжди буде довжиною 6 символів
+  }
+
+const button = document.querySelector(".change-color");// створюємо кнопку
+const colorSpan = document.querySelector('.color');// створюємо span
+
+button.addEventListener("click", () => {
+   const randomColor= getRandomHexColor(); // створюємо посилання на "випадковий колір"
+   document.body.style.backgroundColor = randomColor; // Колір фону всієї сторінки
+   colorSpan.textContent = randomColor; //  Background color: значення кольору 
+   colorSpan.style.color = randomColor; //Необов'язково: встановити колір тексту проміжку таким самим випадковим кольором - але це вказано в ДЗ
+});
+
+
+
+
+
+//Math.floor є вбудованою функцією в JavaScript, яка використовується для округлення числа вниз до найближчого цілого числа. 
+//Це означає, що функція завжди повертає найбільше ціле число, менше або дорівнює даному числу.
