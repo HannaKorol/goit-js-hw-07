6f3502276edd24bf92286400b4c69fa0c8beb84d
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-07-02 15:45:48 +0200

    commit task 4

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,13 @@
-const userName = document.querySelector("#name-input");
-const greeting = document.querySelector("#name-output");
+const userName = document.querySelector("#name-input"); // силаємось на input
+const greeting = document.querySelector("#name-output"); // силаємось на span
 
-userName.addEventListener("input", handleInput)
+userName.addEventListener("input", handleInput) // додаємо слухача для input 
 
-function handleInput(event) {
-    const userNameEvent = event.currentTarget.value;
+function handleInput(event) { // event - це об"єкт який є у кожній функції яка обробляє подію, він зберігає абсолютно всю інформацію про подію яка тільки що відбулась.
+    const userNameEvent = event.currentTarget.value; // Отримується значення введеного тексту
+    // currentTarget - властивість події яка зберігає посилання на елемент на якому висить обробник. В нашому випадку це поле вводу userName(значення яке стоїть перед крапкою методу userName.addEventListener)
+    //.value - отримати значення input,textarea, select
     console.log(event);
-    greeting.textContent = userNameEvent || "Anonymous";
+    greeting.textContent = userNameEvent || "Anonymous"; // якщо значення userNameEvent є "порожнім" (тобто, "", null, undefined, 0, NaN або false), то використовується значення "Anonymous".Інакше буде використано значення userNameEvent.
 };
 
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault(); // обов"язково для того, щоб відмінити поведінку браузера за замовчуванням,  
+    //для подіЇ сабміту - це перевазантаження сторінки
+
+    console.log(event);
+    console.log(event.currentTarget);
+
+const elements = event.currentTarget.elements;
+    //в формі є elements - це властивість для роботи з елементами форми, там зберігаються всі інтерактивні елементи форми: name, email i  comment
+
+const info = {
+    email:
+    
+}
+
+
+}
