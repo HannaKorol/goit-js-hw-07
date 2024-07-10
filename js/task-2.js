b120d2395f5bf35587dbd13a25dc130546b95f06
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-07-10 16:22:14 +0200

    commit edited task 2 (added 2 versions of solution to this task)

--- js/task-2.js
+++ js/task-2.js
@@ -27,13 +27,49 @@ const images = [
   }
 ];
 
-// 1) Знаходимо пустий "ul" список для додавання посилань
-const galleryImgs = document.querySelector("ul.gallery");
 
-// 2) Створюємо функцію для створення нового елемента li
-const createGalleryListItem = () => document.createElement('li');
+// 1) Створення картинок з HTML-розміткою (з елементом списку li)
+function createGalleryImgs(images) {
+  return images.map((item) => 
+    `<li class ="gallery-item">
+  <img src="${item.url}" alt="${item.alt}">
+  </li>`
+).join("");
+};
 
-// 3) Створення картинок 
+// 2) Знаходимо в HTML пустий "ul" список для додавання посилань
+    const galleryList = document.querySelector("ul.gallery");
+
+//3) Вставка згенерованої HTML-розмітки всередину пустого "ul" c класом gallery
+galleryList.insertAdjacentHTML ("beforeend", createGalleryImgs(images));
+
+
+//*innerHTML -  добре підходить для швидкої зміни вмісту елемента в DOM(з перезаписом існуючого вмісту!) коли безпека і продуктивність не є критичними аспектами. Краще не використовувати бо врвзливий до XSS атак.
+//*insertAdjacentHTML — це метод, який дозволяє вставляти HTML-код у визначене місце в DOM-дереві без перезапису існуючого вмісту. Це ефективний спосіб динамічно оновлювати DOM, додаючи нові елементи.("beforebegin""afterbegin""beforeend""afterend")
+
+//--------------------------------------------------------------------------------------------------------------
+
+//*Варіант 2
+
+// 1) Створення li i картинок і додавання їх до батьківського елемента за допомогою appendChild. Цей підхід менш схильний до XSS-атак і може бути більш продуктивним, оскільки не вимагає перерендерингу всього HTML-вмісту.
+/* images.forEach(image => {
+  const listItem = document.createElement("li"); // створюємо елемент списку li
+  const img = document.createElement("img"); // створюємо параметри картинок
+  img.src = image.url;
+  img.alt = image.alt;
+  img.width = 360;
+  img.height = 300;
+  listItem.appendChild(img); // додаємо картинку до елементу списку li
+  galleryList.appendChild(listItem); // додаємо до списку ul створені li
+}); */
+
+//XSS (Cross-Site Scripting) — це тип вразливості в безпеці веб-додатків, який дозволяє зловмисникам вставляти шкідливий скрипт на сторінки, які бачать інші користувачі. Цей скрипт може виконувати різні небажані дії, такі як крадіжка особистих даних, перенаправлення користувачів на фішингові сайти або виконання дій від імені користувача без його відома.
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//*Варіант 3 але він довший і виконання більш заплутане!
+
+/* // 1) Створення картинок 
 const createGalleryImgs = (images) =>
   images.map((item) => {
       const imgEl = document.createElement("img"); // створення картинок
@@ -44,17 +80,21 @@ const createGalleryImgs = (images) =>
       return imgEl; // вивід в консоль - бо це функція
     });
 
+    
+    // 2) Знаходимо пустий "ul" список для додавання посилань
+const galleryImgs = document.querySelector("ul.gallery");
+
+// 3) Створюємо функцію для створення нового елемента li
+const createGalleryListItem = () => document.createElement('li');
+
+
 // 4) Групування картинок в одне ціле
     const galleryEls = createGalleryImgs(images);
-    console.log(galleryEls); 
+    console.log(...galleryEls); 
 
     // 5) Створення нового елемента li і додавання картинок до нього
     const galleryListItem = createGalleryListItem();
-galleryListItem.append(...galleryEls); //*6) Додавання картинок до нового елемента li
+galleryListItem.append(...galleryEls); //6) Додавання картинок до нового елемента li
 
 // 7) Додаємо новий елемент li до батьківського елемента
-galleryImgs.appendChild(galleryListItem);
-
-
-
-
+galleryImgs.appendChild(galleryListItem); */
